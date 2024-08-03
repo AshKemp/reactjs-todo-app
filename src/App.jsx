ba9e55fdb4cbcbf5239554e257685ced53c8240a
AshKemp <kmuraliashwin@gmail.com>
2024-08-03 16:02:04 +0530

    1. install material ui 2. render todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+/* eslint-disable no-unused-vars */
+import { useEffect } from "react";
+import { useState } from "react";
+import classes from "./styles.module.css";
+import TodoItem from "./components/todo-item";
+
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  async function fetchListOfTodos() {
+    try {
+      setLoading(true);
+      const apiResponse = await fetch("https://dummyjson.com/todos");
+      const result = await apiResponse.json();
+      console.log(result);
+      if (result?.todos && result?.todos.length > 0) {
+        setTodos(result?.todos);
+        setLoading(false);
+        setErrorMessage("");
+      } else {
+        setTodos([]);
+        setLoading(false);
+        setErrorMessage("");
+      }
+    } catch (error) {
+      console.log(error);
+      setErrorMessage("Some error occurred");
+    }
+  }
+
+  useEffect(() => {
+    fetchListOfTodos();
+  }, []);
   return (
-    <div>
-      <h1>Simple Todo App using Material UI</h1>
+    <div className={classes.mainWrapper}>
+      <h1 className={classes.headerTitle}>Simple Todo App using Material UI</h1>
+      <div className={classes.todoListWrapper}>
+        {todos && todos.length > 0
+          ? todos.map((todo, index) => <TodoItem todo={todo} key={index} />)
+          : null}
+      </div>
     </div>
   );
 }
